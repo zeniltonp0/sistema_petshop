d0b5e496bf02ad9f1d47d721a4ce777ab2c4c06e
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-21 09:46:11 -0300

    pet-4: criando a entidade Pet e a sua migration

---
+++ database/migrations/2025_08_21_123658_create_pets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('especie');
+            $table->string('raca')->nullable();
+            $table->enum('sexo', ['macho', 'femea']);
+            $table->date('data_nascimento')->nullable();
+            $table->string('foto_pet')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
