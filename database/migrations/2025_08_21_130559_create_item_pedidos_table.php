d9412035d1e2cce010146097f072cb28e11d6878
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-21 10:12:32 -0300

    pet-4: criando os models Produto e ItemPedido e suas migrations

---
+++ database/migrations/2025_08_21_130559_create_item_pedidos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\{Pedido, Produto};
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Pedido::class)->constrained('pedidos', 'id')->onDelete('cascade');
+            $table->foreignIdFor(Produto::class)->constrained('produtos', 'id')->onDelete('cascade');
+            $table->integer('quantidade');
+            $table->decimal('preco_unitario', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_pedidos');
+    }
+};
