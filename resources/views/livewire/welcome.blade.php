d146fc0d06853dff2475e97a5dd531006513953c
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-22 19:44:14 -0300

    PET-7: criado componente de side bar para navegacao

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,3 +1,11 @@
 <div>
-    teste
+    <x-side-bar>
+        <x-slot:brand>Logo</x-slot:brand>
+        <x-side-bar.separator text="Principal" /> 
+            <x-side-bar.item text="Home" icon="home" :route="route('welcome')" />
+            <x-side-bar.item text="Meus Pets" icon="heart" route="#" />
+            <x-side-bar.item text="Loja" icon="shopping-bag" route="#" />
+            <x-side-bar.item text="Agendar Serviços" icon="calendar" route="#" />
+        <x-side-bar.separator text="Conta" /> 
+    </x-side-bar>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Welcome::class);
+Route::get('/', Welcome::class)->name('welcome');
