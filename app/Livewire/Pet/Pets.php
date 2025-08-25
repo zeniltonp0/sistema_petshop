e4c3ab41e76cd9e069428685a4b9233b37f09ece
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-25 18:39:56 -0300

    PET-7: renderizando o componente card

--- app/Livewire/Pet/Pets.php
+++ app/Livewire/Pet/Pets.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\Pet;
 
-use Livewire\Attributes\Layout;
+use Livewire\Attributes\{Computed, Layout};
 use Livewire\Component;
 
 #[Layout('components.layouts.app')]
 class Pets extends Component
 {
+    #[Computed]
+    public function pets()
+    {
+        return auth()->user()->pets;
+    }
     public function render()
     {
         return view('livewire.pet.pets');
--- resources/views/livewire/pet/pets.blade.php
+++ resources/views/livewire/pet/pets.blade.php
@@ -1,3 +1,5 @@
-<div>
-    oi
+<div class="w-96 h-96 mt-4">
+    <x-card>
+        pet
+    </x-card>
 </div>
