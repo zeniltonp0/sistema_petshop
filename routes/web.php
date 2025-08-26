2664b16a27e2cd6578a0c3d9cf0e30a9261ce325
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-26 10:11:17 -0300

    PET-8: fazendo ajustes nos arquivos de teste

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,10 @@ use App\Livewire\Pet\Pets;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('/register', Register::class)->name('register');
-    Route::get('/login', Login::class)->name('login');
-});
+Route::get('/register', Register::class)->name('register');
+Route::get('/login', Login::class)->name('login');
+Route::get('/', Welcome::class)->name('welcome');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', Welcome::class)->name('welcome');
     Route::get('/pets', Pets::class)->name('pets');
 });
