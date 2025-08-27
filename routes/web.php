c951d5eeda4ad222082f9e922c88faa314e088b0
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-27 09:02:50 -0300

    PET-8: agrupando as rotas por guest e auth

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use App\Livewire\Pet\Pets;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', Register::class)->name('register');
-Route::get('/login', Login::class)->name('login');
-Route::get('/', Welcome::class)->name('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', Register::class)->name('register');
+    Route::get('/login', Login::class)->name('login');
+});
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', Welcome::class)->name('welcome');
     Route::get('/pets', Pets::class)->name('pets');
 });
