777a2dbc09786764e466f0698aa451686551a12c
Zenilton Pereira <zeniltonp0@gmail.com>
2025-08-28 07:57:57 -0300

    PET-8: listando os pets do usuario logado

--- resources/views/livewire/pet/pets.blade.php
+++ resources/views/livewire/pet/pets.blade.php
@@ -1,5 +1,13 @@
 <div class="w-96 h-96 mt-4">
-    <x-card>
-        {{-- {{ $this->pets }} --}}
-    </x-card>
+    @foreach ($this->pets as $pet)
+        <div>
+            <x-card image="{{ $pet->foto_pet }}">
+                <span>{{ $pet->nome }}</span>
+                <span>{{ $pet->data_nascimento }}</span>
+                <span>{{ $pet->especie }}</span>
+                <span>{{ $pet->raca }}</span>
+                <span>{{ $pet->sexo }}</span>
+            </x-card>
+        </div>
+    @endforeach
 </div>
