1ea94ff8f45461e08a75bf6ee9511f1488bda70e
Zenilton Pereira <zeniltonp0@gmail.com>
2025-12-10 21:31:17 -0300

    PET-12: criando mode, factory e seeder de raça e espécie

---
+++ app/Models/Raca.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Raca extends Model
+{
+    /** @use HasFactory<\Database\Factories\RacaFactory> */
+    use HasFactory;
+}
